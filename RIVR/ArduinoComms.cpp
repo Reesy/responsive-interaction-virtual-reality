8ce4241cd037e09337efb2e6f99bf65d588572e4
James Rees <james.rees@outlook.com>
2015-02-08 15:05:20 +0000

    Update to initial template, Includes an example Arduino script.

---
+++ RIVR/ArduinoComms.cpp
@@ -0,0 +1,24 @@
+#include "ArduinoComms.h"
+#include <stdio.h>
+#include "SerialClass.h"
+#include <string>
+
+ArduinoComms::ArduinoComms()
+{
+    //ctor
+}
+
+ArduinoComms::~ArduinoComms()
+{
+    //dtor
+}
+
+
+/*
+    ArdNumber is the number of the Arduino, MotorNum is the number of initialised motors
+*/
+void ArduinoComms::Initializer(int ArdNumber, int MotorNum){
+
+
+
+}
---
+++ RIVR/ArduinoComms.h
@@ -0,0 +1,15 @@
+#ifndef ARDUINOCOMMS_H
+#define ARDUINOCOMMS_H
+
+
+class ArduinoComms
+{
+    public:
+        ArduinoComms();
+        virtual ~ArduinoComms();
+        void Initializer(int, int);
+    protected:
+    private:
+};
+
+#endif // ARDUINOCOMMS_H
