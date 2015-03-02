ca0d7e78a06a8e6c2b299ece94f62a8b1332fd02
James Rees <james.rees@outlook.com>
2015-03-02 01:10:54 +0000

    Removed cubes.

--- main.cpp
+++ main.cpp
@@ -134,15 +134,7 @@ int main()
     
     glm::vec3 cubePositions[] = {
         glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+       
     };
     
     
@@ -270,7 +262,7 @@ int main()
         
         // Draw container
         glBindVertexArray(VAO);
-        for(GLuint i = 0; i < 10; i++)
+        for(GLuint i = 0; i < 1; i++)
         {
             glm::mat4 model;
             model = glm::translate(model, cubePositions[i]);
