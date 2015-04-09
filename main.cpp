00e06f0020c46294119a07fafd5928dd84acf62a
James Rees <james.rees@outlook.com>
2015-04-09 14:45:18 +0100

    Added a generate call to key objects under the space key, this is basically an object update call.

--- main.cpp
+++ main.cpp
@@ -439,6 +439,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, GL_TRUE);
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS){
         keyA.isUpper = !keyA.isUpper;
+         keyA.generate();
     }
     if (key == GLFW_KEY_UP && action == GLFW_PRESS){
         testz += 0.1;
