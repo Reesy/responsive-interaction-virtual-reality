07157c53df88edba464bcdb2647e9dc921cd4a07
James Rees <James.rees@outlook.com>
2015-04-16 01:05:16 +0100

    modified for 3 pins, it now takes commands of the form >x,x,x  where > begins where X can be from 0 to 9 (degrees of power) and the commas are used to switch between pins.

--- blink_test/blink_test.ino
+++ blink_test/blink_test.ino
@@ -12,9 +12,16 @@ int result = 0;
 int result2 = 0;
 
 int ledPin = 9;
+int ledPin2 = 6;
+int ledPin3 = 3;
 int val = 0;   
+int pins[3] = {9, 6, 3};
+int vals[3];
 
+
+int current = 0;
 void setup() {
+    
     Serial.begin(9600);
     pinMode(ledPin, OUTPUT);
     
@@ -33,37 +40,45 @@ void loop () {
             currentlyReading = false;
             Serial.println("Reading disengaged");
         }
+        if(val == ','){
+            current++;
+            if(current == 3){
+              current = 0;
+            }
+        }
         if(currentlyReading == true){
            
             if(val == '1'){
-                result = map(1, 1, 9, 1, 255);
+                vals[current] = map(1, 1, 9, 1, 255);
             }
             if(val == '2'){
-                result = map(2, 1, 9, 1, 255);
+                vals[current] = map(2, 1, 9, 1, 255);
             }
             if(val == '3'){
-                result = map(3, 1, 9, 1, 255);
+                vals[current] = map(3, 1, 9, 1, 255);
             }
             if(val == '4'){
-                result = map(4, 1, 9, 1, 255);
+                vals[current] = map(4, 1, 9, 1, 255);
             }
             if(val == '5'){
-                result = map(5, 1, 9, 1, 255);
+                vals[current] = map(5, 1, 9, 1, 255);
             }
             if(val == '6'){
-                result = map(6, 1, 9, 1, 255);
+                vals[current] = map(6, 1, 9, 1, 255);
             }
             if(val == '7'){
-                result = map(7, 1, 9, 1, 255);
+                vals[current] = map(7, 1, 9, 1, 255);
             }
             if(val == '8'){
-                result = map(8, 1, 9, 1, 255);
+                vals[current] = map(8, 1, 9, 1, 255);
             }
             if(val == '9'){
-                result = map(9, 1, 9, 1, 255);
+                vals[current] = map(9, 1, 9, 1, 255);
             }
             
-             analogWrite(ledPin, result);  
+             analogWrite(ledPin, vals[0]);  
+             analogWrite(ledPin2, vals[1]);  
+             analogWrite(ledPin3, vals[2]);  
       }
       
     }
